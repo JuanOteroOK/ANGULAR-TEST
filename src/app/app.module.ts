e5469df0687238cf253c1b2d0ae551dab1f29002
JuanOteroOk <juaan.otero@hotmail.com>
2021-06-15 13:44:04 -0300

    backup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PruebaComponent } from './prueba/prueba.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PruebaComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
