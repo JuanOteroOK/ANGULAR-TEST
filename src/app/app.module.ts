185589ab162304cfe4bba084595aeddc65e0718e
JuanOteroOk <juaan.otero@hotmail.com>
2021-06-16 20:37:26 -0300

    actualizacion160621

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {APP_BASE_HREF} from '@angular/common';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { PagesModule } from './pages/pages.module';
@@ -17,7 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule
   
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: ''}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
